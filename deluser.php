03fab1a2547e3779255eaabe46409e03884ce2d4
nietutejszy <scid.coc1275@gmail.com>
2024-02-26 19:05:47 +0100

    Update deluser.php

--- deluser.php
+++ deluser.php
@@ -1,7 +1,7 @@
 <?php
     $user=$_POST['deluser'];
     $connect=mysqli_connect( "localhost", "root","", "cingciong");
-    $sql= "ALTER TABLE DELETE FROM users_data where login= '$user'";
+    $sql= "DELETE FROM user_data where login= '$user'";
     mysqli_query($connect, $sql);
     mysqli_close($connect);
 ?>
