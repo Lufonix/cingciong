821ee03ec03dd0d58966cea1473d39d259aad32a
IGIXX <159962151+IGIXX@users.noreply.github.com>
2024-02-26 18:25:47 +0100

    Add files via upload

--- cingciong 2.0.sql
+++ cingciong 2.0.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 26 Lut 2024, 18:05
+-- Czas generowania: 26 Lut 2024, 18:25
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.1.12
 
@@ -24,60 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `comments`
---
-
-CREATE TABLE `comments` (
-  `CommentID` int(11) NOT NULL,
-  `ContentID` int(11) DEFAULT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  `CommentText` text DEFAULT NULL,
-  `CommentDate` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `content`
---
-
-CREATE TABLE `content` (
-  `ContentID` int(11) NOT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  `FileName` varchar(255) DEFAULT NULL,
-  `Description` text DEFAULT NULL,
-  `PublicationDate` date DEFAULT NULL,
-  `MediaLocation` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `followers`
---
-
-CREATE TABLE `followers` (
-  `FollowID` int(11) NOT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  `ObserwowanyUserID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `likes`
---
-
-CREATE TABLE `likes` (
-  `LikeID` int(11) NOT NULL,
-  `ContentID` int(11) DEFAULT NULL,
-  `UserID` int(11) DEFAULT NULL,
-  `LikeDate` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Struktura tabeli dla tabeli `users`
 --
 
@@ -87,43 +33,13 @@ CREATE TABLE `users` (
   `Email` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Pastome` varchar(255) NOT NULL,
-  `RegistrationDate` date DEFAULT NULL
+  `RegistrationDate` date DEFAULT NULL,
+  `IsAdmin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Indeksy dla zrzutów tabel
 --
-
---
--- Indeksy dla tabeli `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`CommentID`),
-  ADD KEY `ContentID` (`ContentID`),
-  ADD KEY `UserID` (`UserID`);
-
---
--- Indeksy dla tabeli `content`
---
-ALTER TABLE `content`
-  ADD PRIMARY KEY (`ContentID`),
-  ADD KEY `UserID` (`UserID`);
-
---
--- Indeksy dla tabeli `followers`
---
-ALTER TABLE `followers`
-  ADD PRIMARY KEY (`FollowID`),
-  ADD KEY `UserID` (`UserID`),
-  ADD KEY `ObserwowanyUserID` (`ObserwowanyUserID`);
-
---
--- Indeksy dla tabeli `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`LikeID`),
-  ADD KEY `ContentID` (`ContentID`),
-  ADD KEY `UserID` (`UserID`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -136,59 +52,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `comments`
---
-ALTER TABLE `comments`
-  MODIFY `CommentID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `content`
---
-ALTER TABLE `content`
-  MODIFY `ContentID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `likes`
---
-ALTER TABLE `likes`
-  MODIFY `LikeID` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`ContentID`) REFERENCES `content` (`ContentID`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`);
-
---
--- Ograniczenia dla tabeli `content`
---
-ALTER TABLE `content`
-  ADD CONSTRAINT `content_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`);
-
---
--- Ograniczenia dla tabeli `followers`
---
-ALTER TABLE `followers`
-  ADD CONSTRAINT `followers_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `followers_ibfk_2` FOREIGN KEY (`ObserwowanyUserID`) REFERENCES `users` (`id`);
-
---
--- Ograniczenia dla tabeli `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`ContentID`) REFERENCES `content` (`ContentID`),
-  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
