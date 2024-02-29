8cfac310bee48b11d6cc4b88743cb9026bc7d8df
String_Man420 <80958952+Lufonix@users.noreply.github.com>
2024-02-29 17:53:16 +0100

    Delete deluser.php

--- deluser.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $user=$_POST['deluser'];
-    $connect=mysqli_connect( "localhost", "root","", "cingciong");
-    $sql= "DELETE FROM user_data where login= '$user'";
-    mysqli_query($connect, $sql);
-    mysqli_close($connect);
-?>
