f972b4779a5170df5ca50feefde9c5d2a50f5875
String_Man420 <glina4422@wp.pl>
2024-03-01 17:52:52 +0100

    exdi

--- tools.php
+++ tools.php
@@ -16,46 +16,44 @@ if(!isset($_SESSION['user'])){
 </head>
 <body>
 
-    <div id="topbar">
-        <div id="batony">
-            <button>Użytkownicy</button>
-            <button>Filmy</button>
-            <button>Komentarze</button>
-            <button>[...]</button>
-            <button>[...]</button>
-        </div>
+    <div class="topbar">
 
-        <div id="prabatony">
-            <div></div><a id="logo"href="cingciong.php"><img src="images/logo-alt.png" alt="logo-cingciong"></a>
-            <div></div><a id="logout" href="logout.php"><button>Wyloguj</button></a>
+    
+        <div class="baty1">
+            <div class="baton1">Użytkownicy</div>
+            <div class="baton1">Filmy</div>
+            <div class="baton1">Komentarze</div>
+            <div class="baton1">Wyloguj</div>
+            <div class="baton1"></div>
         </div>
-        
+            
+
+
     </div>
 
     
-<main>
-    
-</main>
+    <main>
+    <div class="wybuzy">
+        Wybierz użytkownika<br>
+
+    <form action="" method="post">
+                <select name="seluser" id="topsel">
+                    <?php
+                        $connect = mysqli_connect('localhost', 'root', '', 'cingciong');
+                        $sql =  "SELECT login FROM user";
+                        $result = mysqli_query($connect, $sql);
+                        while($x = mysqli_fetch_row($result)){
+                            echo "<option value='".$x[0]."'>". $x[0]."</option>";
+                        }
+                        mysqli_close($connect);
+                    ?>
+                </select>
+                <input type="submit" value="Wybierz">
+                </form>
+    </div>
+    </main>
 
 <script src="scripts/js2.js"></script>
 </body>
 </html>
 
-<div id="wybuzy">
-    Wybierz użytkownika<br>
-        
-<form action="" method="post">
-            <select name="seluser" id="topsel">
-                <?php
-                    $connect = mysqli_connect('localhost', 'root', '', 'cingciong');
-                    $sql =  "SELECT login FROM user";
-                    $result = mysqli_query($connect, $sql);
-                    while($x = mysqli_fetch_row($result)){
-                        echo "<option value='".$x[0]."'>". $x[0]."</option>";
-                    }
-                    mysqli_close($connect);
-                ?>
-            </select>
-            <input type="submit" value="Wybierz">
-            </form>
-</div>
