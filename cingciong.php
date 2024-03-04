7e7245e0e767e739cef36717baebdccef8461255
String_Man420 <glina4422@wp.pl>
2024-03-04 16:05:30 +0100

    STOPY

--- cingciong.php
+++ cingciong.php
@@ -38,56 +38,62 @@ if(!isset($_SESSION['user'])){
 
         </div>
 
-        <div id="tyktyk">
-            <a href="tools.php"><button>Panel</button></a>
+        <?php
+
+        require_once "database.php";
+        $kw = "SELECT video.url, user.login, video.title, video.description, comments.content, Users_comment.login FROM video JOIN user ON video.user_id = user.id LEFT JOIN comments ON video.id = comments.video_id LEFT JOIN user AS Users_comment ON comments.user_id = Users_comment.id;";
+        $ress = mysqli_query($con, $kw);
+        $res = mysqli_fetch_row($ress);
+        echo ("
+        <div id='tyktyk'>
+            <a href='tools.php'><button>Panel</button></a><br>
+            <p>$res[0]</p>
         </div>
 
-        <div id="prawy">
+        <div id='prawy'>
             
-            <div id="user_data">
+            <div id='user_data'>
                 
-                <div class="user_avatar">
-                    <img src="images/avatar-def.png" alt="Avatar">
+                <div class='user_avatar'>
+                    <img src='images/avatar-def.png' alt='Avatar'>
                 </div>
             
-                <div class="user_name">
-                    <h3> 	&#9830; Andrzej</h3>
+                <div class='user_name'>
+                    <h3> 	&#9830; $res[1]</h3>
                 </div>
                 
             </div>
 
-        <div class="video_info">
+        <div class='video_info'>
 
-            <div class="video_title">
-                <p>10000000 KOLOROWYCH PIŁEK W WANNIE MOJEJ ZMARŁEJ BABCI BOŻENKI</p>
+            <div class='video_title'>
+                <p>$res[2]</p>
             </div>
             
-            <div class="video_description">
-                <p>#pilka#wanna#babcia#rak#bozena#mamzamiarpopelnicsamobojstwo#dajserduszko</p>
+            <div class='video_description'>
+                <p>$res[3]</p>
             </div>
             
         </div>
             
-            <div class="comments">
-
-                <div class="comments_title">Komentarze</div>
-
-                    <div class="comment">
-                        <div class="comment_name">PolJulecqua2011</div>
-                        <div class="comment_value">
-                            Te kolorowe piłki som super i wgl super filmik! Moja babcia terz zmarzła na raka płuc ale miała na imnie Tadeusz.   
+            <div class='comments'>
+            
+                <div class='comments_title'>Komentarze</div>
+                ");
+                while ($res1 = mysqli_fetch_row($ress)){
+                    echo ("
+                    <div class='comment'>
+                        <div class='comment_name'>$res1[5]</div>
+                        <div class='comment_value'>
+                            $res1[4]
                         </div>
                     </div>
 
-                    <div class="comment">
-                        <div class="comment_name">Adamoooo1278</div>
-                        <div class="comment_value">
-                            Mam nadzieje ze Julcia bendzie wrzucana do tego basenu w nastepnym CingCiongu. Mam nadzieie ze bendzie w samych majtach. ps muj pies ma na imie gustaw   
-                        </div>
-                    </div>
-
+           
+                ");
+            }
+            ?>
             </div>
-            
             <!-- <textarea id="comment_write" name="commentarz" cols="110" rows="7"></textarea> -->
             
         </div>
